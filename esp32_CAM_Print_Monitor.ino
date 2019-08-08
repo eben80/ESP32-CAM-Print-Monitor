056b9c15576222362aca7c1ff8ae3295a9e83c90
Eben van Ellewee <sbmail246@gmail.com>
2019-08-08 08:12:30 +0200

    Add OTA code

--- esp32_CAM_Print_Monitor.ino
+++ esp32_CAM_Print_Monitor.ino
@@ -1,5 +1,8 @@
 #include <esp_camera.h>
 #include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <soc/soc.h>
 #include <soc/rtc_cntl_reg.h>
 // #include <HardwareSerial.h>
@@ -16,7 +19,6 @@
 //#define CAMERA_MODEL_M5STACK_PSRAM
 #define CAMERA_MODEL_AI_THINKER
 
-
 //Config Start
 const char *KNOWN_SSID[] = {"Belkin", "Mi Phone", "AndroidAP"};
 const char *KNOWN_PASSWORD[] = {"Fixui27d69!", "12345678", "7b95ad53f0c9"};
@@ -26,7 +28,6 @@ const int KNOWN_SSID_COUNT = sizeof(KNOWN_SSID) / sizeof(KNOWN_SSID[0]);
 
 //Flash Pin
 #define ledPin 4
-
 
 #ifdef __cplusplus
 extern "C"
@@ -175,7 +176,6 @@ void setup()
   //s->set_framesize(s, FRAMESIZE_QVGA);
   s->set_framesize(s, FRAMESIZE_SVGA); //v12345vtm
 
-
   // ----------------------------------------------------------------
   // WiFi.scanNetworks will return the number of networks found
   // ----------------------------------------------------------------
@@ -251,6 +251,56 @@ void setup()
   Serial.println(WiFi.localIP());
 
   blink();
+
+  // Start OTA Config
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname("PrintCamDev1");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  // End OTA Config
+
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() {
+        Serial.println("\nEnd");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
+
   startCameraServer();
 
   Serial.print("Camera Ready! Use 'http://");
@@ -274,6 +324,6 @@ void loop()
     }
     Serial.println("WiFi Reconnected");
   }
+  ArduinoOTA.handle();
   delay(1000);
-
 }
